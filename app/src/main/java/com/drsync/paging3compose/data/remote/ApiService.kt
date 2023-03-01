41be96bf41bff288a9d48b8f6fcd58f1943113d1
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2023-03-01 09:05:11 +0700

    add : unsplash api interface

---
+++ app/src/main/java/com/drsync/paging3compose/data/remote/ApiService.kt
@@ -0,0 +1,24 @@
+package com.drsync.paging3compose.data.remote
+
+import com.drsync.paging3compose.BuildConfig
+import com.drsync.paging3compose.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImages(
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+}
